b5db20b8758c5b8b6d0b9a7400d7f9e8f79c8e56
janoleko <janole.koslik@gmail.com>
2022-05-05 20:01:24 +0200

    surface

--- R/surface.R
+++ R/surface.R
@@ -1,20 +1,21 @@
 #' Create a surface plot for any function
 #'
-#' @param xlim
-#' @param ylim
-#' @param fun
-#' @param colors
-#' @param showscale
-#' @param showlegend
-#' @param xlab
-#' @param ylab
-#' @param zlab
+#' @param expr an expression or a function containing x and y as variables
+#' @param xlim A vector of limits for the x-axis
+#' @param ylim A vector of limits for the y-axis
+#' @param colors A vector of two colors (can also be the same)
+#' @param showscale Option for showing the colorscale
+#' @param showlegend Option for showing the legend
+#' @param xlab x-axis label
+#' @param ylab y-axis label
+#' @param zlab y-axis label
+#' @param n number of points where function is evalutated (actually n^2)
 #'
-#' @return
+#' @return Returns a interactive 3D surface plot
 #' @export
 #'
 #' @examples
-surface = function(expr, xlim = c(0,20), ylim = c(0,20),
+surface = function(expr, xlim = c(0,40), ylim = c(0,40),
                    colors = c("#ff6c59", "#ffd859"),
                    showscale = FALSE, showlegend = FALSE,
                    xlab = "x", ylab = "y", zlab = "z",
@@ -22,8 +23,13 @@ surface = function(expr, xlim = c(0,20), ylim = c(0,20),
 
   sexpr = substitute(expr)
 
-  f = function(x,y){
+  if (is.name(sexpr)) {
+    f = expr
+  }
+  else{
+    f = function(x,y){
     eval(sexpr)
+    }
   }
 
   x.seq = seq(xlim[1], xlim[2], length.out = n)
